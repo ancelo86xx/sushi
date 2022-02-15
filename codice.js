e1e766b884fb82778144e3cfe77abaf1a92c52be
angelo <bob@alice.it>
2022-02-16 00:17:31 +0100

    Creazione funzioni separate per ogni prodotto

--- codice.js
+++ codice.js
@@ -1,11 +1,19 @@
 
 function initCart () {
     if(!window.localStorage.getItem('cart'))
+        /* Creo tre carrelli specifici per ogni prodotto
+        window.localStorage.setItem('cartDragon', '[]')
+        window.localStorage.setItem('cartCalifornia', '[]')
+        window.localStorage.setItem('cartDynamite', '[]')
+        */
         window.localStorage.setItem('cart', '[]')
 }
-
+        
 function addCartDragon (nome, qnt){
+    /*let cart = JSON.parse(window.localStorage.getItem('cartDragon'))*/
+    
     let cart = JSON.parse(window.localStorage.getItem('cart'))
+
     let obj = {
         nome,
         qnt
@@ -19,19 +27,28 @@ function addCartDragon (nome, qnt){
                 cart[i].qnt = newqnt
                 console.log(cart[i])                                  
           }
-
-            }
-       
-  window.localStorage.setItem('cart', JSON.stringify(cart))
+          if(cart.length > 3){
+            cart.pop(obj)
+        }
+  }
+  window.localStorage.setItem('cart', JSON.stringify(cart)) 
+  /*
+  window.localStorage.setItem('cartDragon', JSON.stringify(cart))
+  */
+ 
 }
 
 function addCartCalifornia (nome, qnt){
-    let cart = JSON.parse(window.localStorage.getItem('cart'))
+    /*
+        let cart = JSON.parse(window.localStorage.getItem('cartCalifornia'))
+        */
+        let cart = JSON.parse(window.localStorage.getItem('cart'))
     let obj = {
         nome,
         qnt
     }
     cart.push(obj)
+    
     
         for (i=0; i<cart.length; i++){
             if (cart[i].nome == "California Roll" ){
@@ -40,20 +57,26 @@ function addCartCalifornia (nome, qnt){
                 cart[i].qnt = newqnt
                 console.log(cart[i])                                  
           }
-   
+          if(cart.length > 3 ){
+            cart.pop(obj)
+        }
           
             }
-       
-  window.localStorage.setItem('cart', JSON.stringify(cart))
+            window.localStorage.setItem('cart', JSON.stringify(cart)) 
+            /*     
+  window.localStorage.setItem('cartCalifornia', JSON.stringify(cart))
+  */
 }
 
 function addCartDynamite (nome, qnt){
-    let cart = JSON.parse(window.localStorage.getItem('cart'))
+   /* let cart = JSON.parse(window.localStorage.getItem('cartDynamite')) */
+   let cart = JSON.parse(window.localStorage.getItem('cart'))
     let obj = {
         nome,
         qnt
     }
     cart.push(obj)
+
     
         for (i=0; i<cart.length; i++){
             if (cart[i].nome == "Dynamite Roll" ){
@@ -62,11 +85,14 @@ function addCartDynamite (nome, qnt){
                 cart[i].qnt = newqnt
                 console.log(cart[i])                                  
             }
-            if(cart.length = 3){
-                cart.pop(obj)
+             if(cart.length > 3){
+                 cart.pop(obj)
+             }
             }
-            }
-       
-  window.localStorage.setItem('cart', JSON.stringify(cart))
+
+            window.localStorage.setItem('cart', JSON.stringify(cart)) 
+            /*
+  window.localStorage.setItem('cartDynamite', JSON.stringify(cart))
+  */
 }
 
