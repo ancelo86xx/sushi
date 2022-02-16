46022cbb93ce40a1d254c92c4f6f41811c9ed45f
angelo <bob@alice.it>
2022-02-16 11:51:18 +0100

    Upload Pagamento, svuota carrello, elimina elemento

--- codice.js
+++ codice.js
@@ -1,98 +1,59 @@
 
 function initCart () {
     if(!window.localStorage.getItem('cart'))
-        /* Creo tre carrelli specifici per ogni prodotto
-        window.localStorage.setItem('cartDragon', '[]')
-        window.localStorage.setItem('cartCalifornia', '[]')
-        window.localStorage.setItem('cartDynamite', '[]')
-        */
-        window.localStorage.setItem('cart', '[]')
+    window.localStorage.setItem('cart', '[]')
 }
-        
-function addCartDragon (nome, qnt){
-    /*let cart = JSON.parse(window.localStorage.getItem('cartDragon'))*/
-    
-    let cart = JSON.parse(window.localStorage.getItem('cart'))
 
-    let obj = {
-        nome,
-        qnt
+function addCart (nome, qnt){
+    if(qnt == '' || isNaN(qnt)) qnt = 1
+
+    let cart = JSON.parse(window.localStorage.getItem('cart'))
+    if (!cart){
+        initCart()
     }
-    cart.push(obj)
-    
-        for (i=0; i<cart.length; i++){
-            if (cart[i].nome == "Dragon Roll" ){
-                let newqnt = cart[i].qnt
-                newqnt = newqnt + 1
-                cart[i].qnt = newqnt
-                console.log(cart[i])                                  
-          }
-          if(cart.length > 3){
-            cart.pop(obj)
+    let found = false
+    for (i=0; i<cart.length; i++){
+        if (nome == cart[i].nome){
+            cart[i].qnt += parseInt(qnt)
+            found = true
         }
-  }
-  window.localStorage.setItem('cart', JSON.stringify(cart)) 
-  /*
-  window.localStorage.setItem('cartDragon', JSON.stringify(cart))
-  */
+    }
+    console.log(qnt, parseInt(qnt))
+    if(!found){
+        cart.push({
+            nome,
+            qnt: parseInt(qnt)
+        })
+    }
+    
+    window.localStorage.setItem('cart', JSON.stringify(cart))          
  
 }
 
-function addCartCalifornia (nome, qnt){
-    /*
-        let cart = JSON.parse(window.localStorage.getItem('cartCalifornia'))
-        */
-        let cart = JSON.parse(window.localStorage.getItem('cart'))
-    let obj = {
-        nome,
-        qnt
+function getCart(){
+    let cart = JSON.parse(window.localStorage.getItem('cart'))
+    let riga = ''
+    let body = document.getElementById('bodyTable')
+    console.log(body)
+    for (i=0; i<cart.length; i++){
+      riga += '<tr><td>' + cart[i].nome + '</td><td>' + cart[i].qnt + '</td><td><button onclick="removeItem('+i+')">elimina</button></td></tr>'
     }
-    cart.push(obj)
-    
-    
-        for (i=0; i<cart.length; i++){
-            if (cart[i].nome == "California Roll" ){
-                let newqnt = cart[i].qnt
-                newqnt = newqnt + 1
-                cart[i].qnt = newqnt
-                console.log(cart[i])                                  
-          }
-          if(cart.length > 3 ){
-            cart.pop(obj)
-        }
-          
-            }
-            window.localStorage.setItem('cart', JSON.stringify(cart)) 
-            /*     
-  window.localStorage.setItem('cartCalifornia', JSON.stringify(cart))
-  */
+    console.log(riga)
+    body.innerHTML = riga
 }
 
-function addCartDynamite (nome, qnt){
-   /* let cart = JSON.parse(window.localStorage.getItem('cartDynamite')) */
-   let cart = JSON.parse(window.localStorage.getItem('cart'))
-    let obj = {
-        nome,
-        qnt
-    }
-    cart.push(obj)
-
-    
-        for (i=0; i<cart.length; i++){
-            if (cart[i].nome == "Dynamite Roll" ){
-                let newqnt = cart[i].qnt
-                newqnt = newqnt + 1
-                cart[i].qnt = newqnt
-                console.log(cart[i])                                  
-            }
-             if(cart.length > 3){
-                 cart.pop(obj)
-             }
-            }
-
-            window.localStorage.setItem('cart', JSON.stringify(cart)) 
-            /*
-  window.localStorage.setItem('cartDynamite', JSON.stringify(cart))
-  */
+function cartEmpty(){
+    window.localStorage.removeItem('cart')
+    window.localStorage.setItem('cart', '[]')
+    getCart()
 }
 
+function removeItem (i){
+    let cart = JSON.parse(window.localStorage.getItem('cart'))
+    cart.splice(i,1);
+    window.localStorage.setItem('cart', JSON.stringify(cart))   
+    getCart()
+   
+  
+    
+}
